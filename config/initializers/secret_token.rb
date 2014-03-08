aec97ea848393072e7a416ee30e85bdb9e87adc1
Dimitris A <aiwa78@gmail.com>
2014-03-08 18:27:26 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thefoodreview::Application.config.secret_key_base = '34d596e01948ef7e250aa14eea70a58e9fa83db90bda5f72ff84e1aa9bf6f111ab062c6cd5240e50385c375759426a02b07e0d68acb25849fe8619e56012862b'
